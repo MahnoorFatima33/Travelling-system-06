a1b82515c5d14c18fcef166c9bf37b239c157587
Mahnoorarshad71 <95930578+Mahnoorarshad71@users.noreply.github.com>
2023-10-01 16:04:00 +0500

    Faulty Fronend removed

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductComponent } from './product/product.component';
-
-export const routes: Routes = [  
-    // redirect to `first-component`
-    {path: "Product2", component: ProductComponent}
-];
